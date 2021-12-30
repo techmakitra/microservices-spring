b77378eccc815f14578ba2d970cfc3e01536de0c
dev <dev>
2021-12-30 16:18:34 +0200

    +

--- product-service/src/main/java/teachmakitra/microservices/spring/common/model/PageableResult.java
+++ product-service/src/main/java/teachmakitra/microservices/spring/common/model/PageableResult.java
@@ -1,15 +1,16 @@
 package teachmakitra.microservices.spring.common.model;
 
 import lombok.Getter;
-import teachmakitra.microservices.spring.common.exception.ServiceException;
+import lombok.Value;
 
 import java.util.List;
 @Getter
+@Value
 public class PageableResult<T> {
-    private long offset;
-    private long limit;
-    private long totalCount;
-    private List<T> data;
+    long offset;
+    long limit;
+    long totalCount;
+    List<T> data;
 
     public PageableResult(long offset, long limit, long totalCount, List<T> data) {
         this.offset = offset;
