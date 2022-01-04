6bb2bec211ef4f9be48ff3e46c0d2de818fc8731
dev <dev>
2022-01-04 23:37:23 +0200

    +

--- product-service/src/main/java/teachmakitra/microservices/spring/common/configuration/TracingConfig.java
+++ product-service/src/main/java/teachmakitra/microservices/spring/common/configuration/TracingConfig.java
@@ -1,13 +1,12 @@
 package teachmakitra.microservices.spring.common.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import teachmakitra.microservices.spring.common.tracing.CustomTracingTagsInterceptor;
 
 @Configuration
-public class TracingConfig  implements WebMvcConfigurer {
+public class TracingConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
